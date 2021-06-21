ec981bc8c10a93c909b6716929afeb737aa02256
Ivan Viter <ivanviter@i.ua>
2021-06-21 17:58:24 +0300

    Generate Cars controller

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,17 @@
+class CarsController < ApplicationController
+  def index
+  end
+
+  def edit; end
+
+  def update
+    #@car = Car.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  def destroy
+  end
+end
