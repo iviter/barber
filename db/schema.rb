680be2d23a7c7f5913f5b9a4540832dc74905770
Ivan Viter <ivanviter@i.ua>
2021-06-26 15:26:51 +0300

    Change format of fields in Car table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_121401) do
+ActiveRecord::Schema.define(version: 2021_06_26_122305) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 2021_06_26_121401) do
     t.string "type"
     t.string "brand"
     t.string "model"
-    t.string "price"
-    t.string "year"
+    t.integer "price"
+    t.integer "year"
     t.string "country"
     t.string "condition"
     t.string "fuel_type"
