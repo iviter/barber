73184c3e787d67e7f0845f4f3b869d85fc62d4e5
Ivan Viter <ivanviter@i.ua>
2021-07-04 14:20:27 +0300

    Update Ads controller with all endpoints

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -5,15 +5,43 @@ class AdvertisementsController < ApplicationController
 
   def new
     @ad = Advertisement.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @ad }
+    end
   end
 
   def show
+    @ad = Advertisement.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @ad }
+    end
   end
 
   def edit
+    @ad = Advertisement.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @ad }
+    end
   end
 
   def update
+    @ad = Advertisement.find(params[:id])
+
+    respond_to do |format|
+      if @ad.update_attributes(ad_params)
+        format.html { redirect_to advertisements_path, notice: 'Advertisement was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @ad.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def create
@@ -31,6 +59,13 @@ class AdvertisementsController < ApplicationController
   end
 
   def destroy
+    @ad = Advertisement.find(params[:id])
+    @ad.destroy
+
+    respond_to do |format|
+      format.html { redirect_to advertisements_path }
+      format.json { head :no_content }
+    end
   end
 
   private
