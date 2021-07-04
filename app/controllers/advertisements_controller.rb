8b952d70fa5304f9df717afbb9fc7de06f9d0cc1
Ivan Viter <ivanviter@i.ua>
2021-07-04 12:32:59 +0300

    Rename Ad column

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -17,8 +17,25 @@ class AdvertisementsController < ApplicationController
   end
 
   def create
+    @ad = Advertisement.new(ad_params)
+
+    respond_to do |format|
+      if @ad.save
+        format.html { redirect_to ads_path, notice: 'Advertisement was successfully created.' }
+        format.json { render json: @ad, status: :created, location: @ad }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @ad.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def ad_params
+    params.require(:advertisement).permit(:ad_type, :title, :description, :user_id, :car_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_084811) do
+ActiveRecord::Schema.define(version: 2021_07_04_091241) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_04_084811) do
   end
 
   create_table "advertisements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "type"
+    t.string "ad_type"
     t.string "rating"
     t.datetime "published_at"
     t.integer "user_id"
