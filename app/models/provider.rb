0ccb3e07cfa28f5c96a18a93f542c2e4eff94b0d
Ivan Viter <ivanviter@i.ua>
2021-07-17 12:07:29 +0300

    Fix associations

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,5 @@
 class Provider < User
   has_many :advertisements, foreign_key: :user_id, dependent: :destroy
   has_one :user_profile, foreign_key: :user_id
-  has_many :cars, through: :advertisements, foreign_key: :user_id
   has_many :comments, foreign_key: :user_id, dependent: :destroy
 end
