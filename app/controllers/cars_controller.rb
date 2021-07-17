48afb26c8a8e12df80043bc0124175f0331f9957
Ivan Viter <ivanviter@i.ua>
2021-07-17 11:00:03 +0300

    Remove unnecessary json format from CarsController

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,65 +3,37 @@ class CarsController < ApplicationController
 
   def index
     @cars = Car.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @cars }
-    end
   end
 
   def new
     @car = Car.new
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @car }
-    end
   end
 
   def show
     @car = Car.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @car }
-    end
   end
 
   def edit
     @car = Car.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @car }
-    end
   end
 
   def update
     @car = Car.find(params[:id])
 
-    respond_to do |format|
-      if @car.update_attributes(car_params)
-        format.html { redirect_to car_path, notice: 'Car was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
-      end
+    if @car.update_attributes(car_params)
+      redirect_to car_path, notice: 'Car was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   def create
     @car = Car.new(car_params)
 
-    respond_to do |format|
-      if @car.save
-        format.html { redirect_to cars_path, notice: 'Car was successfully created.' }
-        format.json { render json: @car, status: :created, location: @car }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
-      end
+    if @car.save
+      redirect_to cars_path, notice: 'Car was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
@@ -69,10 +41,7 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
     @car.destroy
 
-    respond_to do |format|
-      format.html { redirect_to cars_path, notice: 'Car was successfully deleted.' }
-      format.json { head :no_content }
-    end
+    redirect_to cars_path, notice: 'Car was successfully deleted.'
   end
 
   private
